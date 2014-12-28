7ff5dd307ecab6fc162ce09dd0cc9a1bab1d78c3
Keiichi Lindley <digizn@gmail.com>
2014-12-28 12:00:44 -0700

    Adding Adpex Calulator

--- app/models/Traces.php
+++ app/models/Traces.php
@@ -108,9 +108,17 @@ class Trace {
 
 		$apdex['score'] = ( ( count($apdex['satifactory']['records']) + ( count($apdex['tolerable']['records']) / 2 ) ) / 2 );
 
-		$apdex['satifactory']['count'] = count($apdex['satifactory']['records']);
-		$apdex['tolerable']['count'] = count($apdex['tolerable']['records']);
-		$apdex['frusturating']['count'] = count($apdex['frusturating']['records']);
+		if ( $apdex['satifactory']['records'] ) 
+
+			$apdex['satifactory']['count'] = count($apdex['satifactory']['records']);
+
+		if ( $apdex['tolerable']['records'] )
+
+			$apdex['tolerable']['count'] = count($apdex['tolerable']['records']);
+
+		if ( $apdex['frusturating']['records'] )
+
+			$apdex['frusturating']['count'] = count($apdex['frusturating']['records']);
 
 
 		return $apdex;
